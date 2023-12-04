e6ce76095371a21d5d1d21c85d19f45f6e72c6b4
Erob711 <emattroberson@gmail.com>
2023-12-04 13:46:47 -0700

    started items router w/ gets and updates

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,5 @@ const express = require("express");
 const router = express.Router();
 
 // different model routers
-router.use('/sauces', require('./sauces'));
-
+router.use('/items', require('./items'));
 module.exports = router;

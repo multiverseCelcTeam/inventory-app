83a117faf91e49b70065a9738d4722fb5c0dfa24
Chase AK <109045340+achasek@users.noreply.github.com>
2023-12-04 10:40:52 -0700

    lint (#14)

--- server/models/Sauce.js
+++ server/models/Sauce.js
@@ -1,8 +1,8 @@
 const { db, DataTypes } = require('../db/connection');
 
 const Sauce = db.define("sauces", {
-    name: DataTypes.STRING,
-    image: DataTypes.STRING,
+  name: DataTypes.STRING,
+  image: DataTypes.STRING,
 });
 
 module.exports = Sauce;

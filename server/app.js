371c5029888a86093804f18b18808754bd230f1a
achasek <chaseknieper@gmail.com>
2023-12-04 11:08:16 -0700

    test

--- server/app.js
+++ server/app.js
@@ -22,14 +22,15 @@ app.use('/api', require('./routes'));
 
 // 404 handler
 app.use((req, res) => {
-  res.status(404).send({error: '404 - Not Found', message: 'No route found for the requested URL'});
+  res.status(404).send({ error: '404 - Not Found', message: 'No route found for the requested URL' });
 });
 
 // error handling middleware
+// eslint-disable-next-line no-unused-vars
 app.use((error, req, res, next) => {
   console.error('SERVER ERROR: ', error);
   if(res.statusCode < 400) res.status(500);
-  res.send({error: error.message, name: error.name, message: error.message, table: error.table});
+  res.send({ error: error.message, name: error.name, message: error.message, table: error.table });
 });
 
 module.exports = app;

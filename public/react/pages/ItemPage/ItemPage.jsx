ac15f4ba9589c44878eb969e832e9b0b11cfa55c
Erob711 <emattroberson@gmail.com>
2023-12-06 13:32:08 -0700

    some basic styles for ItemPage

--- public/react/pages/ItemPage/ItemPage.jsx
+++ public/react/pages/ItemPage/ItemPage.jsx
@@ -1,30 +1,40 @@
-import { Link, useParams, useNavigate } from 'react-router-dom';
+import { Link, useParams, useNavigate } from "react-router-dom";
 import itemsServices from "../../services/Item";
-
+import "./ItemPage.css";
 const ItemPage = ({ items, setItems }) => {
-    const id = useParams().id
-    const item = items.find((item) => item.id === Number(id));
-    const navigate = useNavigate();
+  const id = useParams().id;
+  const item = items.find((item) => item.id === Number(id));
+  const navigate = useNavigate();
 
-    const handleDelete = (id) => {
-        const filteredItems = items.filter((item) => item !== items.id)
-        setItems(filteredItems);
-        itemsServices.deleteItem(id);
-        navigate('/');
-    }
+  const handleDelete = (id) => {
+    const filteredItems = items.filter((item) => item !== items.id);
+    setItems(filteredItems);
+    itemsServices.deleteItem(id);
+    navigate("/");
+  };
 
-    return (
-        <>
-            <h3>{item.name}</h3>
-            <h3>{item.price}</h3>
-            <h3>{item.description}</h3>
-            <h3>{item.category}</h3>
-            <img src={item.image} alt={item.name} />
-            <Link to={`/editItem/${id}`}>Edit Item</Link>
-            <button onClick={ () => handleDelete(item.id)}>Delete Item</button>
-            <Link to='/'>Back to Main Page</Link>
-        </>
-    )
+  return (
+    <>
+      <div className="page">
+        <div className="item-section">
+          <div className="item">
+            <h3><span className="tag">name</span>{item.name}</h3>
+            <img className="item-image" src={item.image} alt={item.name} />
+            <h3><span className="tag">price</span>${item.price}</h3>
+            <h3><span className="tag">description</span>{item.description}</h3>
+            <h3><span className="tag">category</span>{item.category}</h3>
+
+          </div>
+
+          <div className="buttons">
+            <button id="edit-btn"><Link to={`/editItem/${id}`} className="link">Edit</Link></button>
+            <button onClick={() => handleDelete(item.id)} id="delete-btn">Delete</button>
+            <button><Link to="/" className="link">Back to Main Page</Link></button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ItemPage;

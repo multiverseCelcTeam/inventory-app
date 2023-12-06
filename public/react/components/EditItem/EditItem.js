2de0c9c300dfad1f8aeb255f3ba000340f81968e
SnageTheSnakeMage <101869780+SnageTheSnakeMage@users.noreply.github.com>
2023-12-06 14:14:41 -0700

    super basic form styling

--- public/react/components/EditItem/EditItem.js
+++ public/react/components/EditItem/EditItem.js
@@ -3,6 +3,31 @@ import { useParams, useNavigate } from "react-router-dom";
 import itemServices from '../../services/Item.js';
 
 const EditItem = ({ items, setItems }) => {
+      //Styling
+  const header = {
+      textAlign: "center", 
+      fontSize: "5vh"
+  }
+  const formStyle = {
+      display: "flex",
+      flexDirection: "column",
+      boxSizing: "border-box",
+      justifyContent: "space-between",
+      alignItems: "center",
+      height: "80vh",
+      margin: "5px"
+  }
+  const inputStyle = {
+    display: "flex",
+    height: "10vh",
+    width: "90vw",
+    fontSize: "8vh"
+  }
+  const submitStyle = {
+      height: "15vh",
+      fontSize: "5vh"
+  }
+
     const navigate = useNavigate();
     const id = useParams().id
     const item = items.find((item) => item.id === Number(id));

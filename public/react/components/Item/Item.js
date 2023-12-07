f23c33f1d9438dae0c27477baf28f22afc8b1278
ldvalle3 <127244361+ldvalle3@users.noreply.github.com>
2023-12-07 14:35:10 -0700

    Lissette valle (#46)
    * added .npmrc file
    
    * updated gitignore
    
    * git changes
    
    * home page UI update with navbar
    
    ---------
    
    Co-authored-by: lissettevalle <lissette.valle@airdna.co>

--- public/react/components/Item/Item.js
+++ public/react/components/Item/Item.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './item.css';
 
 export const Item = (props) => {
 
   return <>
+  <div className='link-container'>
+    <div className = "flex-box">
     <Link to={`/${props.item.id}`}>
       <h3>{props.item.name}</h3>
       <img src={props.item.image} alt={props.item.name} />
     </Link>
+    </div>
+    </div>
   </>
 } 
 	

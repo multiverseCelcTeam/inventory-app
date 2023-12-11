d435733921f7c8d8ed43248ca7e203bd916821bd
Erob711 <emattroberson@gmail.com>
2023-12-11 11:41:18 -0700

    prod deployment added

--- public/react/api.js
+++ public/react/api.js
@@ -3,7 +3,7 @@ let apiURL;
 if (process.env.NODE_ENV === 'development') {
   apiURL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 } else {
-  apiURL = '/'
+  apiURL = 'https://inventory-app-d2ik.onrender.com';
 }
 
 export default apiURL;
